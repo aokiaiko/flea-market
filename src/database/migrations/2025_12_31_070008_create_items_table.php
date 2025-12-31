8dbc23cb20b82b72fc0be1c3ea4707b729faadf6
aoki aiko <aiko.24542676@gmail.com>
2026-01-01 07:31:16 +0900

    usersとitemsテーブルを作成

---
+++ src/database/migrations/2025_12_31_070008_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100);
+            $table->string('brand_name',50)->nullable();
+            $table->integer('price');
+            $table->foreignId('user_id')->constrained();
+            $table->text('description');
+            $table->integer('condition');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
